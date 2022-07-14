da1ddd915b63c736308ceca9035ac92054494259
Scotty McMasters <48250605+Scottikus@users.noreply.github.com>
2022-07-14 14:19:23 -0500

    My First saved python program.

---
+++ python/storingVariable.py
@@ -0,0 +1,15 @@
+from unicodedata import name
+
+
+spam = 40
+
+print("Hello World!")
+print("What's your name?")
+nameVar = input()
+
+print("It's good to meet you, " + nameVar)
+print("The length of you name is: ")
+print(len(nameVar))
+print("Spam is: ")
+print(spam)
+
